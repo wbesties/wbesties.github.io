75e565afaccc456cdfe32044ecf8b7626d3e5bc8
Luominai <kevinthepear@gmail.com>
2026-09-21 23:39:07 -0400

    corporate

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173
+  },
   input: {
     main: resolve(import.meta.dirname, 'index.html'),
     luominai: resolve(import.meta.dirname, 'luominai/index.html'),
