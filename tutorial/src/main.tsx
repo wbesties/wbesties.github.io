665b350fea4d3bfd025e1afbbd608d103d5afd7c
Luominai <kevinthepear@gmail.com>
2026-09-24 00:37:25 -0400

    a better way to delay createRoot

--- tutorial/src/main.tsx
+++ tutorial/src/main.tsx
@@ -2,8 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+let clicked = false
+document.getElementById("link1").addEventListener(("click"), (e) => {
+  if (clicked) {
+    return
+  }
+  clicked = true
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
